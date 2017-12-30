0af9c08f638c1910e730bb35b0eed58e65d972c1
Madis Väin <madisvain@gmail.com>
2017-12-30 17:28:39 +0200

    add travis

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -20,7 +20,7 @@ except ImportError:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'madisvain/siteship'
+GITHUB_REPO = 'siteship/siteship-cli'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
